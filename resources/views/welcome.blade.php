8a0da340b1be1a6141cbe963a5ae386be15b3c71
Álvaro Guimarães <alvaroguimaraes@gmail.com>
2016-09-13 23:51:24 -0300

    No button at least for now.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
                 <div class="col-md-9">
                     <h1 itemprop="name">Álvaro Guimarães</h1>
                     <p itemprop="description">PHP Developer, Open Source community collaborator and father of 3 beautiful child.</p>
-                    <p><a class="btn btn-primary btn-lg" href="#" role="button">More about me &raquo;</a></p>
+                    {{--<p><a class="btn btn-primary btn-lg" href="#" role="button">More about me &raquo;</a></p>--}}
                 </div>
             </div>
         </div>
